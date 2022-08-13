4cb7289ccd30a42f6c3f3f00e75c5b7369a83ae1
machadosguilherme <gui_1703@hotmail.com>
2022-08-12 23:47:50 -0300

    Feat: criando os componentes

--- ext/model.py
+++ ext/model.py
@@ -21,3 +21,20 @@ class Usuario(db.Model, UserMixin):
 
     def __str__(self):
         return self.name
+
+class Agendamento(db.Model):
+    __tablename__ = "AGENDAMENTO"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('USUARIOS.id'))
+    nome = db.Column(db.String(length=255), nullable=False)
+    email = db.Column(db.String(length=255), nullable=False)
+    fone = db.Column(db.String(length=50), nullable=False)
+    data_agendamento = db.Column(db.String(length=10), nullable=False)
+    horario_agendamento = db.Column(db.String, nullable=False)
+    colaborador = db.Column(db.String)
+    data_registro = db.Column(db.DateTime, nullable=False)
+    in_confimado = db.Column(db.Boolean)
+    in_realizado =db.Column(db.Boolean)
+
+    def __str__(self):
+        return self.name
--- ext/routes.py
+++ ext/routes.py
@@ -1,12 +1,16 @@
 from http.client import BAD_REQUEST
-from .model import Usuario
+from .model import Usuario, Agendamento
 from ext import login_maneger, db
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import UserMixin, login_required, login_user
+from flask_login import UserMixin, login_required, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def init_app(app):
+    
+    @app.before_first_request
+    def create_db():
+        db.create_all()
 
     @login_maneger.user_loader
     def current_user(id):
@@ -26,7 +30,12 @@ def init_app(app):
             usuario = Usuario.query.filter_by(email=email).first()
             if usuario and check_password_hash(usuario.senha, senha):
                 login_user(usuario)
-                return #TODO Dashboard
+                if usuario.in_admin == 0:
+                    return redirect (url_for('dash_admin', id=usuario.id))
+                elif usuario.in_colaborador == 1:
+                    return redirect (url_for('dash_colaborador', id=usuario.id))
+                else:
+                    return redirect(url_for('dash_cliente', id=usuario.id))
             else:
                 flash('Credências Invalidas.')
                 return redirect(url_for('login'))
@@ -48,6 +57,9 @@ def init_app(app):
             user.fone = request.form['fone']
             user.email = request.form['email']
             user.senha = generate_password_hash(request.form['senha'])
+            user.in_admin = 1
+            user.in_colaborador = 0
+            user.in_ativo = 1
             
             db.session.add(user)
             db.session.commit()
@@ -55,5 +67,56 @@ def init_app(app):
             return redirect(url_for('login'))
         else:
             return BAD_REQUEST
+    
+    @app.route('/dashboard/<id>', methods=['GET', 'POST'])
+    @login_required
+    def dash_cliente(id):
+        usuario = Usuario.query.filter_by(id = id).first()
+        agendamentos = Agendamento.query.filter_by(id = id).count()
+        if usuario.in_admin == 1:
+            if usuario.in_colaborador == 0:
+                return render_template('dash_cliente/dashboard_cliente.html', usuario=usuario.nome, agendamentos=agendamentos)
+        else:
+            return BAD_REQUEST
         
+    
+    @app.route('/dashboard_colaborador/<id>', methods=['GET', 'POST'])
+    @login_required
+    def dash_colaborador(id):
+        usuario = Usuario.query.filter_by(id = id).first()
+        if usuario.in_colaborador == 1:
+            return render_template('dash_colaborador/dashboard_colaborador.html')
+        else:
+            return BAD_REQUEST
+    
+    @app.route('/dashboard_admin/<id>', methods=['GET', 'POST'])
+    @login_required
+    def dash_admin(id):
+        usuario = Usuario.query.filter_by(id = id).first()
+        agendamentos = Agendamento.query.filter_by().count()
+        novos_usuarios = Usuario.query.filter_by().count()
+        if usuario.in_admin == 1:
+            return render_template('dash_admin/dashboard_admin.html',
+                           novos_usuarios=novos_usuarios,
+                           agendamentos=agendamentos)
+        else:
+            return BAD_REQUEST
+ 
+    @app.route('/logout', methods=['GET', 'POST'])
+    @login_required
+    def logout():
+        logout_user()
+        return redirect(url_for('index'))
+    
+    @app.route('/agendamento', methods=['GET', 'POST'])
+    @login_required
+    def agendamento():
+        return render_template('dash_cliente/agendamentos_cliente.html')
         
+    @app.route('/dashboard_admin_teste', methods=['GET', 'POST'])
+    def dash_admin_teste():
+        # usuario = Usuario.query.filter_by(id = id).first()
+        # if usuario.in_admin == 1:
+        return render_template('dash_admin/dashboard_admin.html')
+        # else:
+        #     return BAD_REQUEST
